5fb88329b2270b576e0a4e07f6f4d3974330887c
njllucy <njllucy1221@gmail.com>
2026-03-10 16:03:44 +0600

    fixed image issues

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,7 @@ const projects = [
     description:
       "AI based face recognition system built with OpenCV and TensorFlow for detecting and recognizing faces in real-time.",
     tech: ["Python", "OpenCV", "TensorFlow"],
-    image: "/src/components/images/facerecognition.png",
+    image: "/images/facerecognition.png",
     github:
       "https://github.com/njllucy/Face_Recognition-OpenCv-Tensorflow-.git",
     // live: "",
@@ -50,7 +50,7 @@ const projects = [
       "A networking project demonstrating Ddos Attack Simulation & Detection",
     tech: ["C++", "Networking Concepts"],
     github: "https://github.com/njllucy/Computer-Network-Project-.git",
-    image: "/public/images/CN.png",
+    image: "/images/CN.png",
     color: "from-teal-300 to-teal-800",
 
   },
@@ -61,7 +61,7 @@ const projects = [
     tech: ["JavaScript", "HTML", "CSS"],
     github: "https://github.com/njllucy/Eat-Split-js-project-.git",
     live:"https://njllucy.github.io/Eat-Split-js-project-/",
-    image: "/public/images/Eat & split.png",
+    image: "/images/Eat & split.png",
     color: "from-amber-400 to-amber-800",
 
   },
@@ -72,7 +72,7 @@ const projects = [
     tech: ["HTML", "Tailwind CSS", "JavaScript", "Swiper.js"],
     github: "https://github.com/njllucy/StoryNest-Book-website-.git",
     live:"https://njllucy.github.io/StoryNest-Book-website-/",
-    image: "/public/images/bookwebsite.png",
+    image: "/images/bookwebsite.png",
     color: "from-purple-200 to-purple-700",
 
   }
